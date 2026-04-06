d707274b185dfe74837241e1c70214674209ed4a
LuisDuran04 <lduran3@student.gsu.edu>
2026-04-06 12:35:46 -0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ window._config = {
     cognito: {
         userPoolId: 'us-east-1_0VLXEILnv', // e.g. us-east-1_uXboG5pAb
         userPoolClientId: '4r0tb5i3sjt9uoe9ou2sq8phc7', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        region: '' // e.g. us-east-1
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
